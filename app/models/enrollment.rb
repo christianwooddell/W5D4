dc7c5e61fbfec35674f349dfc50368237710f2dc
Christian <cwooddel@u.rochester.edu>
2021-01-28 14:22:57 -0500

    initial commit

---
+++ app/models/enrollment.rb
@@ -0,0 +1,11 @@
+class Enrollment < ApplicationRecord
+    belongs_to :course,
+        primary_key: :id,
+        class_name: :Course,
+        foreign_key: :course_id
+        
+    belongs_to :user,
+        primary_key: :id, 
+        class_name: :User,
+        foreign_key: :student_id
+end
